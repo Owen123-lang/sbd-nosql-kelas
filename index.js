0c6fb2428b2218d64912b9fe67a884b3e17c224f
Rowen Rodotua Harahap <150427908+OWENNNN123@users.noreply.github.com>
2025-05-07 11:06:03 +0700

    test docker
    test docker

--- index.js
+++ index.js
@@ -43,4 +43,16 @@ if (require.main === module) {
     app.listen(PORT, () => console.log(`🚀 Server started at port:${PORT}`));
 }
 
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('✅ Connected to MongoDB Atlas'))
+  .catch((err) => {
+    console.error('❌ MongoDB connection error:', err.message);
+    process.exit(1); // supaya container mati kalau gak bisa konek
+  });
+  
+
 module.exports = app;
